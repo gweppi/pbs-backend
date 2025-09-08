cc289175b30a860d0a891ba6696502d89d635bd8
Jesper Dinger <dev@jesperdinger.com>
2025-09-08 11:06:41 +0200

    logs work now

--- src/utils.ts
+++ src/utils.ts
@@ -6,9 +6,9 @@ export default async function getDocument(url: string) {
    if (!response.ok) {
       // Response was not successful, return error
       console.error(`The requested webpage returned a wrong statuscode: ${response.status}`)
-      console.debug(`response headers: ${response.headers}`)
+      console.debug(`response headers: ${JSON.stringify(response.headers)}`)
       console.debug(`response body: ${await response.text()}`)
-      console.debug(`request headers: ${request.headers}`)
+      console.debug(`request headers: ${JSON.stringify(request.headers)}`)
       throw new Error("Failed retrieving the document.")
    }
    const html = await response.text();
